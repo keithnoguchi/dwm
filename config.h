29da110a845abbe4ab146996613e377e8827ee0a
Kei Nohguchi <kei@nohguchi.com>
2016-04-25 22:51:18 -0700

    config.h: Lighter color.

--- config.h
+++ config.h
@@ -6,15 +6,15 @@ static const char *fonts[] = {
 };
 static const char dmenufont[]       = "monospace:size=16:bold";
 static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
+static const char normbgcolor[]     = "#ffffff";
+static const char normfgcolor[]     = "#222222";
 static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int showbar            = 0;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
