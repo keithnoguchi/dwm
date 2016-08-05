241b399c078549bcb2866ad220e7f9877d43814c
Kei Nohguchi <kei@nohguchi.com>
2016-08-04 18:58:59 -0700

    config.h: Make the font to 36 on ocean

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=24:bold"
+	"monospace:size=36:bold"
 };
 static const char dmenufont[]       = "monospace:size=16:bold";
 static const char normbordercolor[] = "#444444";
